e9d043fc42543f08b0a43aae9522b49dafa91b93
pranshu <srivastavapranshu2@gmail.com>
2023-01-26 23:12:17 +0530

    Repos

--- src/components/Repos.jsx
+++ src/components/Repos.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import { GithubContext } from '../context/context'
+import { Examplechart, Column3D, Pie3D, Bar3D, Doughnut2D } from '../charts/Index';
 const Repos = () => {
+  const {githubRepos} = React.useContext(GithubContext);
   return (
-    <div>Repos</div>
+    <div>
+      <Examplechart/>
+    </div>
   )
 }
 
