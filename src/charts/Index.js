aad8b7748dc63af43d391b64fb7464dd24b759cf
pranshu <srivastavapranshu2@gmail.com>
2023-01-26 23:10:51 +0530

    added charts to display repo data

---
+++ src/charts/Index.js
@@ -0,0 +1,7 @@
+import Examplechart from "./Examplechart";
+import Column3D from "./Column3D";
+import Pie3D from "./Pie3D";
+import Bar3D from "./Bar3D";
+import Doughnut2D from "./Doughnut2D";
+
+export { Examplechart, Column3D, Pie3D, Bar3D, Doughnut2D };
