915a1e2d3e04a560a86223829d5e6490fb9859ef
pranshu <srivastavapranshu2@gmail.com>
2023-01-29 22:34:48 +0530

    Most used language

--- src/charts/Pie3D.jsx
+++ src/charts/Pie3D.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+// STEP 1 - Include Dependencies
+// Include react
+import React from "react";
 
-const Pie3D = () => {
-  return (
-    <div>Pie3D</div>
-  )
-}
+// Include the react-fusioncharts component
+import ReactFC from "react-fusioncharts";
 
-export default Pie3D
+// Include the fusioncharts library
+import FusionCharts from "fusioncharts";
+
+// Include the chart type
+import Column2D from "fusioncharts/fusioncharts.charts";
+
+// Include the theme as fusion
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
+
+
+// STEP 3 - Creating the JSON object to store the chart configurations
+
+
+const chartComponent = ({data})=>{
+  const chartConfigs = {
+    type: "Pie3d", // The chart type
+    width: "400", // Width of the chart
+    height: "400", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        //Set the chart caption
+        caption: "Languages",
+        theme:"fusion",
+        pieRadius:'45%'
+        },
+      // Chart Data
+      data,
+    }
+  };
+  return (<ReactFC {...chartConfigs} />);
+
+};
+
+export default chartComponent;
--- src/components/Repos.jsx
+++ src/components/Repos.jsx
@@ -3,9 +3,24 @@ import { GithubContext } from '../context/context'
 import { Examplechart, Column3D, Pie3D, Bar3D, Doughnut2D } from '../charts/Index';
 const Repos = () => {
   const {githubRepos} = React.useContext(GithubContext);
+  const languages = githubRepos.reduce((total, item)=>{
+    const {language} = item;
+    if(!language) return total;
+    if(!total[language]){
+      total[language] = {label:language, value:1};
+    }
+    else{
+      total[language] = {...total[language], value:total[language].value+1}
+    }
+    return total;
+  },{})
+  let lang = Object.values(languages).sort((a,b)=>{
+    return b.value-a.value
+  }).slice(0, 4);
   return (
     <div>
-      <Examplechart/>
+      {/* <Examplechart data = {chartData}/> */}
+      <Pie3D data = {lang}/>
     </div>
   )
 }
